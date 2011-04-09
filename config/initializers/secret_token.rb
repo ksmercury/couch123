621afd581559a3891818ca2fcb111e8396f9cd5c
Kim Sarkies <meaculpa123ks@hotmail.com>
2011-04-09 15:40:43 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Couch123::Application.config.secret_token = 'e36a5110c1ebbc660077378ce531dde3f78abd3a85d0d4c005b33449b1d9677537b0b9d0bf8ba0e33083850779f66cd7e18853fa5c68dc748bdebd87dd7e7263'
